02527613d9dad5c75b0074a314ff0bbe03c5dda0
Arturo Lopez <aarturock92@gmail.com>
2017-06-16 09:58:50 -0500

    Create method list in VehiculoController

--- CEMEX.API/Controllers/Catalogos/VehiculoController.cs
+++ CEMEX.API/Controllers/Catalogos/VehiculoController.cs
@@ -1,8 +1,15 @@
-﻿using CEMEX.API.Infrastructure.Core;
+﻿using AutoMapper;
+using CEMEX.API.Infrastructure.Core;
+using CEMEX.API.Models.Catalogos;
+using CEMEX.Data.Extensions.Catalogos;
 using CEMEX.Data.Infrastructure;
 using CEMEX.Data.Repositories;
+using CEMEX.Entidades;
 using CEMEX.Entidades.Catalogos;
 using CEMEX.Entidades.Seguridad;
+using System.Collections;
+using System.Collections.Generic;
+using System.Net;
 using System.Net.Http;
 using System.Web.Http;
 
@@ -22,11 +29,23 @@ namespace CEMEX.API.Controllers.Catalogos
             _vehiculoRepository = vehiculoRepository;
         }
 
-        //[Route("list")]
-        //[HttpGet]
-        //public HttpResponseMessage Get()
-        //{
+        [Route("list")]
+        [HttpGet]
+        public HttpResponseMessage Get(HttpRequestMessage request, 
+                                       ETypeEstatusRegistro estatusRegistro = ETypeEstatusRegistro.Todos)
+        {
+            return CreateHttpResponse(request, () =>
+            {
+                HttpResponseMessage response = null;
 
-        //}
+                IEnumerable<VehiculoViewModel> vehiculosVM = Mapper.Map<IEnumerable<Vehiculo>, 
+                                                                        IEnumerable<VehiculoViewModel>>
+                                                                        (_vehiculoRepository.GetVehiculosByEstatusRegistro(estatusRegistro));
+
+                response = request.CreateResponse(HttpStatusCode.OK, vehiculosVM);
+
+                return response;
+            });
+        }
     }
 }
---
+++ CEMEX.Data/Extensions/Catalogos/VehiculoExtensions.cs
@@ -0,0 +1,47 @@
+﻿using CEMEX.Data.Repositories;
+using CEMEX.Entidades;
+using CEMEX.Entidades.Catalogos;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CEMEX.Data.Extensions.Catalogos
+{
+    public static class VehiculoExtensions
+    {
+        public static IEnumerable<Vehiculo> GetVehiculosByEstatusRegistro(this IEntityBaseRepository<Vehiculo> repositoryVehiculo, ETypeEstatusRegistro estatusRegistro)
+        {
+            IEnumerable<Vehiculo> Vehiculos = null;
+
+            switch (estatusRegistro)
+            {
+                case ETypeEstatusRegistro.Activo:
+                    Vehiculos = repositoryVehiculo.GetAll()
+                                                  .Where(v => v.IdEstatus == (int)ETypeEstatusRegistro.Activo)
+                                                  .ToList();
+                    break;
+                case ETypeEstatusRegistro.Inactivo:
+                    Vehiculos = repositoryVehiculo.GetAll()
+                              .Where(v => v.IdEstatus == (int)ETypeEstatusRegistro.Inactivo)
+                              .ToList();
+                    break;
+                case ETypeEstatusRegistro.Eliminado:
+                    Vehiculos = repositoryVehiculo.GetAll()
+                                                  .Where(v => v.IdEstatus == (int)ETypeEstatusRegistro.Eliminado)
+                                                  .ToList();
+                    break;
+                case ETypeEstatusRegistro.Todos:
+                    Vehiculos = repositoryVehiculo.GetAll()
+                                                  .Where(v => v.IdEstatus == (int)ETypeEstatusRegistro.Activo ||
+                                                              v.IdEstatus == (int)ETypeEstatusRegistro.Inactivo)
+                                                  .ToList();
+                    break;
+                default:
+                    Vehiculos = repositoryVehiculo.GetAll()
+                                                  .ToList();
+                    break;
+            }
+
+            return Vehiculos;
+        }
+    }
+}
